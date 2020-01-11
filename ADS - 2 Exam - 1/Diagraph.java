e3564778839da4a994242a913bda71d75390aa51
Msit@1234 <vivekreddy422.kandula@msitprogram.net>
2020-01-11 13:00:48 +0530

    Exam 1

---
+++ ADS - 2 Exam - 1/Diagraph.java
@@ -0,0 +1,46 @@
+public class Diagraph {
+    Bag<Integer>[] adj;
+    int v;
+    int e;
+    int[] indegree;
+    public Diagraph(int v) {
+        this.v = v;
+        e = 0;
+        adj = (Bag<Integer>[]) new Bag[v];
+        for (int i = 0; i < adj.length; i++) {
+            adj[i] = new Bag();
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        indegree[w++];
+        e++;
+    }
+
+    public int v() {
+        return v;
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < adj.length; i++) {
+            System.out.println(i + " --> " + adj[i]);
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        WordNet wn = new WordNet("synsets.txt", "hypernyms.txt");
+        // System.out.println(wn.ht);
+        // System.out.println(wn.ht2);
+        // System.out.println(wn.ht.size());
+        System.out.println(wn.ht2.size());
+        Diagraph dg = new Diagraph(wn.ht2.size());
+        for (int i = 0; i < wn.ht2.size(); i++) {
+            for (int j = 0; j < wn.ht2.get(i).size(); j++) {
+                dg.addEdge(i, wn.ht2.get(i).get(j));
+            }
+        }
+        System.out.println(dg.e);
+        // dg.printGraph();
+    }
+}
